144488d936275754697e936d2dddfd37fb7c0457
imeubug <im.eubug@gmail.com>
2020-08-15 08:40:41 +0900

    webpack initial setting setup

---
+++ webgame/04-number-baseball/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+    name: 'Number Baseball Setting',
+    devtool: 'eval',
+    mode: 'development',
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+
+    entry: {
+        app: ['./client'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-react'],
+                    plugins: ['react-hot-loader/babel'],
+                },
+            }
+        ],
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].js',
+        publicPath: '/dist/',
+    },
+}
