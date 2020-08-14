8f99ebfe33394f4235ff822e6897bb538139a9c9
Jii Eu <jione.eu@gmail.com>
2020-08-14 16:46:00 +0900

    webpack basic setup done

---
+++ webgame/03-shiritori-webpack/client.jsx
@@ -0,0 +1,6 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+const Shiritori = require('./shiritori');
+
+ReactDOM.render(<Shiritori />, document.querySelector('#root'))
