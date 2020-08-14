87fe5f83bb7e98ce2634cb80d273307f42af66a4
Jii Eu <jione.eu@gmail.com>
2020-08-14 17:42:21 +0900

    gugudan changed using webpack done

---
+++ webgame/02-gugudan-webpack/client.jsx
@@ -0,0 +1,6 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+const GuGuDan = require('./gugudan'); // import gugudan.jsx
+
+ReactDOM.render(<GuGuDan />, document.querySelector('#root'));
