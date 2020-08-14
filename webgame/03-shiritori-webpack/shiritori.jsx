efe5c0a5ddc04c272497aab0fc042e51f2b52e83
Jii Eu <jione.eu@gmail.com>
2020-08-15 07:12:19 +0900

    shiritori class webpack-dev-server, hot-loader example

--- webgame/03-shiritori-webpack/shiritori.jsx
+++ webgame/03-shiritori-webpack/shiritori.jsx
@@ -3,11 +3,53 @@ const { Component } = React;
 
 class Shiritori extends Component {
     state = {
-        text: 'Hello, Webpack!',
+        word: "Jione",
+        value: '',
+        result: '',
+    };
+
+    onSubmitForm = (e) => {
+        e.preventDefault();
+
+        this.state.word = this.state.word.toLowerCase();
+        this.state.value = this.state.value.toLowerCase();
+        if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
+            this.setState({
+                result: 'Correct!',
+                word: this.state.value[0].toUpperCase() + this.state.value.substr(1),
+                value: '',
+            })
+        } else {
+            this.setState({
+                result: 'Wrong!',
+                word: this.state.word[0].toUpperCase() + this.state.word.substr(1),
+                value: '',
+            })
+        }
+
+        this.input.focus();
+    };
+
+    onChangeInput = (e) => {
+        this.setState({ value: e.target.value });
+    };
+
+    input;
+    onRefInput = (c) => {
+        this.input = c;
     };
 
     render() {
-        return <h1>{this.state.text}</h1>;
+        return (
+            <>
+                <div>{ this.state.word }</div>
+                <form onSubmit={this.onSubmitForm}>
+                    <input type="text" ref={this.onRefInput} value={this.state.value} onChange={this.onChangeInput}/>
+                    <button type="submit">Enter!</button>
+                </form>
+                <div>{ this.state.result }</div>
+            </>
+        );
     }
 } 
 
