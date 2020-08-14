014cd2443922db01097d8971e2d380bd810b248d
ipflegen <ipflegen@pm.me>
2020-08-15 07:12:19 +0900

    shiritori class webpack-dev-server, hot-loader example

--- webgame/03-shiritori-webpack/webpack.config.js
+++ webgame/03-shiritori-webpack/webpack.config.js
@@ -1,30 +1,45 @@
-const path = require('path'); 
+const path = require('path');
 
 module.exports = {
-    name: 'shiritori-setting',
-    mode: 'development',
+    name: 'Shiritori Setting',
     devtool: 'eval',
+    mode: 'development',
     resolve: {
         extensions: ['.js', '.jsx'],
     },
+
     entry: {
         app: ['./client'],
     },
 
     module: {
-        rules: [{
-            test: /\.jsx$/,
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env', '@babel/preset-react'],
-                plugins: ['@babel/plugin-proposal-class-properties'],
+        rules: [
+            {
+                test: /\.jsx$/,
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                        ['@babel/preset-env', {
+                            targets: {
+                                browsers: ['> 1% in US']
+                            },
+                            debug: true,
+                        }],
+                        '@babel/preset-react'
+                    ],
+                    plugins: [
+                        '@babel/plugin-proposal-class-properties',
+                        'react-hot-loader/babel'
+                    ],
+                },
             },
-        }],
+        ],
     },
-    
+
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: '[name].js'
+        filename: '[name].js',
+        publicPath: '/dist/',
     },
-    
+
 }
