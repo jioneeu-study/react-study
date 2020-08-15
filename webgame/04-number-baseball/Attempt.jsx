75d8f5c58ba94bf72110fd0baca6e94eb4a2e127
ipflegen <ipflegen@pm.me>
2020-08-15 10:32:39 +0900

    컴포넌트 분리하기

---
+++ webgame/04-number-baseball/Attempt.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Attempt extends Component {
+    render() {
+        return (
+            <li>{this.props.value}</li>
+        )
+    }
+}
+
+export default Attempt;
