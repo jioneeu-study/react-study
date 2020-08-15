110bc1646b2b934e0c757feadcdf2a29ad68b671
Jii Eu <jione.eu@gmail.com>
2020-08-15 11:13:08 +0900

    Attempt component changed using Hooks

--- webgame/04-number-baseball/Attempt.jsx
+++ webgame/04-number-baseball/Attempt.jsx
@@ -1,12 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Attempt extends Component {
-    render() {
-        const { value } = this.props;
-        return (
-        <li key="{this.props.key}"> {value.attempt}<br/>{value.result}</li>
-        )
-    }
-}
+const Attempt = ({value}) => {
+    return (
+        <li> 
+            <div>
+                {value.attempt}
+            </div>
+            <div>
+                {value.result}
+            </div>
+        </li>
+    )
+};
 
 export default Attempt;
