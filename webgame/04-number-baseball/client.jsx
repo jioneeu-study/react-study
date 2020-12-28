e6ec05127bdba531cddb83264c67598c7b472823
Jii Eu <jione.eu@gmail.com>
2020-12-28 20:44:45 +0900

    stashed

--- webgame/04-number-baseball/client.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { hot } from 'react-hot-loader/root';
-
-import NumberBaseball from './numberBaseball';
-const Hot = hot(NumberBaseball);
-
-ReactDOM.render(<Hot />, document.querySelector('#root'));
