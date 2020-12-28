169dde7aab41f469928924fb76f11c81551ed747
ipflegen <ipflegen@pm.me>
2020-12-28 20:44:45 +0900

    stashed

--- webgame/02-gugudan-webpack/class-webpack/client.jsx
+++
@@ -1,6 +0,0 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-
-const GuGuDan = require('./gugudan'); // import gugudan.jsx
-
-ReactDOM.render(<GuGuDan />, document.querySelector('#root'));
