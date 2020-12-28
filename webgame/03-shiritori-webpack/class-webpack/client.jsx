f71ae24ef2f0c6d35e5990f0292792d69bc1aeb4
imeubug <im.eubug@gmail.com>
2020-12-28 20:44:45 +0900

    stashed

--- webgame/03-shiritori-webpack/class-webpack/client.jsx
+++
@@ -1,9 +0,0 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-const { hot } = require('react-hot-loader/root');
-
-const Shiritori = require('./shiritori');
-
-ReactDOM.render(<Shiritori />, document.querySelector('#root'))
-
-export default hot(Shiritori);
--- webgame/03-shiritori-webpack/hook-webpack/client.jsx
+++
@@ -1,8 +0,0 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-const { hot } = require('react-hot-loader/root');
-
-const Shiritori = require('./shiritori');
-const Hot = hot(Shiritori)
-
-ReactDOM.render(<Hot />, document.querySelector('#root'))
