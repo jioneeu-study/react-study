7b1de94d53989216b49f23fde8cad013ced23203
Jii Eu <jione.eu@gmail.com>
2020-12-30 17:39:09 +0900

    Webpack - gugudan

---
+++ inflearn/learning-by-making-web-games/2-shiritori/Shiritori.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+const { Component } = React;
+
+class Shiritori extends Component {
+    state = {
+        first: Math.ceil(Math.random() * 20),
+        second: Math.ceil(Math.random() * 20),
+        value: '',
+        result: '',
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        let a = parseInt(this.state.first);
+        let b = parseInt(this.state.second);
+        let answer = a * b;
+
+        if (parseInt(this.state.value) === answer) {
+            this.setState({
+                first: Math.ceil(Math.random() * 20),
+                second: Math.ceil(Math.random() * 20),
+                value: '',
+                result: <div>CORRECT! <br /> {a} x {b} = {answer} </div>,
+            })
+        } else {
+            this.setState({
+                value: '',
+                result: <div>WRONG! <br /> {a} x {b} is NOT {+this.state.value} </div>,
+            })
+        }
+    };
+
+    onChange = (e) => {
+        this.setState({
+            value: e.target.value,
+        })
+    };
+
+    render() {
+        return (
+            <>
+                <div>{this.state.first} times {this.state.second} is?</div>
+                <form onSubmit={this.onSubmit}>
+                    <input type="number" value={this.state.value} onChange={this.onChange} />
+                    <button>Check!</button>
+                </form>
+                <div>{this.state.result}</div>
+            </>
+        );
+    };
+}
+
+export default Shiritori;
