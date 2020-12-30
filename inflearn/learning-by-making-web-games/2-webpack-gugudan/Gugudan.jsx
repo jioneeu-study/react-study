858e41401916143b55169e571a475a1b8a31faf9
ipflegen <ipflegen@pm.me>
2020-12-30 17:57:03 +0900

    create Gugudan with Webpack-build : both hooks and classes

--- inflearn/learning-by-making-web-games/2-webpack-gugudan/Gugudan.jsx
+++ inflearn/learning-by-making-web-games/2-webpack-gugudan/Gugudan.jsx
@@ -1,53 +1,95 @@
 import React from 'react';
 const { Component } = React;
 
-class Gugudan extends Component {
-    state = {
-        first: Math.ceil(Math.random() * 20),
-        second: Math.ceil(Math.random() * 20),
-        value: '',
-        result: '',
-    };
+/* Hooks */
+const Gugudan = () => {
+    const [first, setFirst] = React.useState(Math.ceil(Math.random() * 20));
+    const [second, setSecond] = React.useState(Math.ceil(Math.random() * 20));
+    const [value, setValue] = React.useState('');
+    const [result, setResult] = React.useState('');
 
-    onSubmit = (e) => {
+    const onSubmitForm = (e) => {
         e.preventDefault();
-        let a = parseInt(this.state.first);
-        let b = parseInt(this.state.second);
+        let a = parseInt(first);
+        let b = parseInt(second);
         let answer = a * b;
 
-        if (parseInt(this.state.value) === answer) {
-            this.setState({
-                first: Math.ceil(Math.random() * 20),
-                second: Math.ceil(Math.random() * 20),
-                value: '',
-                result: <div>CORRECT! <br /> {a} x {b} = {answer} </div>,
-            })
+        if (parseInt(value) === answer) {
+            setFirst(Math.ceil(Math.random() * 20));
+            setSecond(Math.ceil(Math.random() * 20));
+            setValue('');
+            setResult(<div>CORRECT! <br /> {a} x {b} = {answer} </div>);
         } else {
-            this.setState({
-                value: '',
-                result: <div>WRONG! <br /> {a} x {b} is NOT {+this.state.value} </div>,
-            })
+            setValue('');
+            setResult(<div>WRONG! <br /> {a} x {b} is NOT {+value} </div>);
         }
     };
 
-    onChange = (e) => {
-        this.setState({
-            value: e.target.value,
-        })
+    const onChangeInput = (e) => {
+        setValue(e.target.value);
     };
 
-    render() {
-        return (
-            <>
-                <div>{this.state.first} times {this.state.second} is?</div>
-                <form onSubmit={this.onSubmit}>
-                    <input type="number" value={this.state.value} onChange={this.onChange} />
-                    <button>Check!</button>
-                </form>
-                <div>{this.state.result}</div>
-            </>
-        );
-    };
-}
+    return (
+        <>
+            <div>{first} times {second} is?</div>
+            <form onSubmit={onSubmitForm}>
+                <input type="number" value={value} onChange={onChangeInput} />
+                <button>Check!</button>
+            </form>
+            <div>{result}</div>
+        </>
+    )
+
+};
+
+/* Class */
+// class Gugudan extends Component {
+//     state = {
+//         first: Math.ceil(Math.random() * 20),
+//         second: Math.ceil(Math.random() * 20),
+//         value: '',
+//         result: '',
+//     };
+
+//     onSubmit = (e) => {
+//         e.preventDefault();
+//         let a = parseInt(first);
+//         let b = parseInt(second);
+//         let answer = a * b;
+
+//         if (parseInt(value) === answer) {
+//             this.setState({
+//                 first: Math.ceil(Math.random() * 20),
+//                 second: Math.ceil(Math.random() * 20),
+//                 value: '',
+//                 result: <div>CORRECT! <br /> {a} x {b} = {answer} </div>,
+//             })
+//         } else {
+//             this.setState({
+//                 value: '',
+//                 result: <div>WRONG! <br /> {a} x {b} is NOT {+value} </div>,
+//             })
+//         }
+//     };
+
+//     onChange = (e) => {
+//         this.setState({
+//             value: e.target.value,
+//         })
+//     };
+
+//     render() {
+//         return (
+//             <>
+//                 <div>{first} times {second} is?</div>
+//                 <form onSubmit={this.onSubmit}>
+//                     <input type="number" value={value} onChange={this.onChange} />
+//                     <button>Check!</button>
+//                 </form>
+//                 <div>{result}</div>
+//             </>
+//         );
+//     };
+// }
 
 export default Gugudan;
