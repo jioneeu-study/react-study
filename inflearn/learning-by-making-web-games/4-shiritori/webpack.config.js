014990b09dd6bfe4a111f2444ecf5b7080cefa9a
Jii Eu <jione.eu@gmail.com>
2021-01-03 16:04:29 +0900

    webpack-dev-server & react-refresh added

--- inflearn/learning-by-making-web-games/4-shiritori/webpack.config.js
+++ inflearn/learning-by-making-web-games/4-shiritori/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpackRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
     name: 'shiritori-setting',
@@ -17,13 +18,26 @@ module.exports = {
             test: /\.jsx?/,
             loader: 'babel-loader',
             options: {
-                presets: ['@babel/preset-react']
+                presets: ['@babel/preset-react'],
+                plugins: [
+                    'react-refresh/babel'
+                ],
             }
-        }]
+        }],
     },
+
+    plugins: [
+        new webpackRefreshPlugin(),
+    ],
 
     output: { // output
         path: path.join(__dirname, 'dist'),
         filename: 'app.js',
+        publicPath: '/dist/',
     },
+
+    devServer: {
+        publicPath: '/dist/',
+        hot: true,
+    }
 };
