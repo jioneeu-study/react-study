12c9d0c4a6b2bdc195f55136821fb0819983a527
Jii Eu <jione.eu@gmail.com>
2021-01-03 16:33:08 +0900

    끝말잇기 finished

--- inflearn/learning-by-making-web-games/4-shiritori/dictionary.jsx
+++ inflearn/learning-by-making-web-games/4-shiritori/dictionary.jsx
@@ -5,9 +5,6 @@ const key = 'key=B7F84D10A34D0B2FC7C8A19772619E22';
 const url = base + key + '&q=';
 
 function searchResult(word) {
-    if (!word || word === '' || word === undefined || word === NaN) {
-        word = '버그';
-    }
     console.log('checking: ' + word);
 
     return Promise.resolve(axios.get('https://cors-anywhere.herokuapp.com/' + url + word))
